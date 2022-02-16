18909adee67d4a0d62980b7b4acc8ae0b0c5877b
Avinash Kumar Singh <avinashmukeshsingh@outlook.com>
2022-02-16 23:23:50 +0530

    first commit

--- index.js
+++ index.js
@@ -1,66 +1,67 @@
 /*
-
-Need 2 things :
-(1) Show/Display video on homepage
-(2) Search when something is typed in the search-box.
-
+Need 2 Things:
+(1) Show/Display Videos on Homepage
+(2) Search When Something is typed in the search-box
 display()
--Show/Display video on homepage
--Based upon most popular videos
--Should be called directly without any event
-(No Hover, No Click, No On Click)
--on page load
-
-
-search-video()
--Search when something is typed in the search-box.
--Take the "String" typed in search-box on the string
-
-
+- Show/Display Videos on Homepage
+- Based Upon Most Popular Videos
+- Should be Called Directly WITHOUT Any Event 
+(No Hover, No Click , No On Click)
+- On Page Load
+searchVideo()
+- Search When Something is typed in the search-box
+- Take the "String" typed in search-box and search on that string 
 */
 
-let div = document.getElementById("videoiv");
+let div = document.getElementById("videodiv");
 
-// Show/Display video on homepage
+
+// (1) Show/Display Videos on Homepage
+
+
 
 async function display(){
 
+    // q = Popular Videos --> Popular%20Videos (%20 -> Space)
+
+    let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=popular%20videos&key=AIzaSyA00OxUbfdfVAjHn-9l48s0t-n_j7CBbNs&maxResults=25`);
+
+    let data = await res.json();
+    
+    for({id:{videoId}}of data.items)
+    {
+    let videodiv= document.createElement("iframe")
+    videodiv.setAttribute("class","mons")
+    videodiv.src = `https://www.youtube.com/embed/${videoId}`;
+    videodiv.allow = 'fullscreen'
+    div.append(videodiv)
+    }
 
 }
 
 display();
 
-// Search when something is typed in the search-box
+
+
+
+// (2) Search When Something is typed in the search-box
 
 async function searchVideos(){
-    document.getElementById("videoiv").innerHTML="";
+    document.getElementById("videodiv").innerHTML=""; // Empty 
 
-    let video = document.getElementById("video").value;
-    
-    // Search for videos
-/*
-    let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${video}&type=video&key=AIzaSyA00OxUbfdfVAjHn-9l48s0t-n_j7CBbNs&maxResults=25`);
-    let data = await res.json();
+    let query = document.getElementById("video").value; // Search String/ Query String
 
-    for({
-        id:{videoId}}of data.items){
-            let videoiv = document.createElement("iframe")
-            videoiv.setAttribute("class","mons")
-            videoiv.src = `https://www.youtube.com/embed/${videoId}`;
-            videoiv.allow = 'fullscreen'
-            div.append(videoiv)
-        }
-}
-*/
-let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${video}&type=video&key=AIzaSyA00OxUbfdfVAjHn-9l48s0t-n_j7CBbNs&maxResults=25`);
+//search for videos
+let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&key=AIzaSyA00OxUbfdfVAjHn-9l48s0t-n_j7CBbNs&maxResults=25`);
 let data = await res.json();
 
-for({
-id:{videoId}}of data.items){
+for({id:{videoId}}of data.items)
+{
 let videodiv= document.createElement("iframe")
 videodiv.setAttribute("class","mons")
 videodiv.src = `https://www.youtube.com/embed/${videoId}`;
 videodiv.allow = 'fullscreen'
 div.append(videodiv)
+}
 
 }
