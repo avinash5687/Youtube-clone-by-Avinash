d36fcbeeecebcf379e41fddea56b7ddf04cb4025
Avinash Kumar Singh <avinashmukeshsingh@outlook.com>
2022-02-19 16:30:14 +0530

    commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ searchVideo()
 let div = document.getElementById("videodiv");
 
 
-// (1) Show/Display Videos on Homepage
+//  Show/Display Videos on Homepage
 
 
 
@@ -30,11 +30,11 @@ async function display(){
     
     for({id:{videoId}}of data.items)
     {
-    let videodiv= document.createElement("iframe")
-    videodiv.setAttribute("class","mons")
-    videodiv.src = `https://www.youtube.com/embed/${videoId}`;
-    videodiv.allow = 'fullscreen'
-    div.append(videodiv)
+        let videodiv= document.createElement("iframe")
+        videodiv.setAttribute("class","mons")
+        videodiv.src = `https://www.youtube.com/embed/${videoId}`;
+        videodiv.allow = 'fullscreen'
+        div.append(videodiv)
     }
 
 }
@@ -44,24 +44,24 @@ display();
 
 
 
-// (2) Search When Something is typed in the search-box
+//    Search When Something is typed in the search-box
 
 async function searchVideos(){
     document.getElementById("videodiv").innerHTML=""; // Empty 
 
     let query = document.getElementById("video").value; // Search String/ Query String
 
-//search for videos
-let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&key=AIzaSyA00OxUbfdfVAjHn-9l48s0t-n_j7CBbNs&maxResults=25`);
-let data = await res.json();
+    //search for videos
+    let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&key=AIzaSyA00OxUbfdfVAjHn-9l48s0t-n_j7CBbNs&maxResults=25`);
+    let data = await res.json();
 
-for({id:{videoId}}of data.items)
-{
-let videodiv= document.createElement("iframe")
-videodiv.setAttribute("class","mons")
-videodiv.src = `https://www.youtube.com/embed/${videoId}`;
-videodiv.allow = 'fullscreen'
-div.append(videodiv)
-}
+    for({id:{videoId}}of data.items)
+    {
+        let videodiv= document.createElement("iframe")
+        videodiv.setAttribute("class","mons")
+        videodiv.src = `https://www.youtube.com/embed/${videoId}`;
+        videodiv.allow = 'fullscreen'
+        div.append(videodiv)
+    }
 
 }
